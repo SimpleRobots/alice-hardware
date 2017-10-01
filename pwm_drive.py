42bbd1f9c129783c064187edb54a077c8cec9077
Michael Fuerst <mail@michaelfuerst.de>
2017-10-01 11:56:54 +0200

    Switched to non pwm digital driver (since it produces less load on hardware.
    A digital driver is sufficient for ai purposes and makes things simpler.

--- pwm_drive.py
+++ pwm_drive.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 
-LEFT_POWER = 11
-RIGHT_POWER = 3
+PWM_FREQ = 40
+LEFT_PWM = 11
+RIGHT_PWM = 3
 
 LEFT_DIR_1 = 13
 LEFT_DIR_2 = 15
@@ -11,32 +12,34 @@ RIGHT_DIR_2 = 7
 
 MAX_SPEED_LEFT = 0.1142 * 2
 MAX_SPEED_RIGHT = 0.1142 * 2
+MAX_ALLOWED_POWER = 0.5
 WHEELBASE = 0.2
-
-POWER_THRESHOLD = 0.1
 
 
 class Driver(object):
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(LEFT_POWER, GPIO.OUT)
-        GPIO.setup(RIGHT_POWER, GPIO.OUT)
+        GPIO.setup(LEFT_PWM, GPIO.OUT)
+        GPIO.setup(RIGHT_PWM, GPIO.OUT)
         GPIO.setup(LEFT_DIR_1, GPIO.OUT)
         GPIO.setup(LEFT_DIR_2, GPIO.OUT)
         GPIO.setup(RIGHT_DIR_1, GPIO.OUT)
         GPIO.setup(RIGHT_DIR_2, GPIO.OUT)
 
-        GPIO.output(LEFT_POWER, GPIO.LOW)
-        GPIO.output(RIGHT_POWER, GPIO.LOW)
-        
+        self.left_pwm = GPIO.PWM(LEFT_PWM, PWM_FREQ)
+        self.right_pwm = GPIO.PWM(RIGHT_PWM, PWM_FREQ)
+
+        self.left_pwm.start(0)
+        self.right_pwm.start(0)
+
     def set_speed(self, v_left, v_right):
         # Cap the max speed, so that you do not set values larger than allowed
         v_left = max(min(v_left, MAX_SPEED_LEFT), -MAX_SPEED_LEFT)
         v_right = max(min(v_right, MAX_SPEED_RIGHT), -MAX_SPEED_RIGHT)
 
         # Transform speed to PWM
-        v_left /= MAX_SPEED_LEFT
-        v_right /= MAX_SPEED_RIGHT
+        v_left /= MAX_SPEED_LEFT / MAX_ALLOWED_POWER
+        v_right /= MAX_SPEED_RIGHT / MAX_ALLOWED_POWER
 
         # Controll the motor direction correctly
         if v_left >= 0:
@@ -56,18 +59,11 @@ class Driver(object):
             v_right = -v_right
 
         # Actually set the duty for the motor
-        if v_left > POWER_THRESHOLD:
-            GPIO.output(LEFT_POWER, GPIO.HIGH)
-        else:
-            GPIO.output(LEFT_POWER, GPIO.LOW)
-
-        if v_right > POWER_THRESHOLD:
-            GPIO.output(RIGHT_POWER, GPIO.HIGH)
-        else:
-            GPIO.output(RIGHT_POWER, GPIO.LOW)
+        self.left_pwm.ChangeDutyCycle(v_left)
+        self.right_pwm.ChangeDutyCycle(v_right)
 
     def kill(self):
         # When killing reseting the pwm so the motors deinitialize
-        GPIO.output(LEFT_POWER, GPIO.LOW)
-        GPIO.output(RIGHT_POWER, GPIO.LOW)
+        self.left_pwm.stop()
+        self.right_pwm.stop()
         GPIO.cleanup()
